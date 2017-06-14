6d97633e58437fb75550333fc532f7be6c86ddb6
hannahboy24 <hannahboy24@gmail.com>
2017-06-13 21:56:50 -0700

    sdasfdasf

--- application/views/template/dashboard_footer.php
+++ application/views/template/dashboard_footer.php
@@ -45,8 +45,13 @@
 	<script src="<?php echo base_url();?>assets/plugins/datatables/media/js/jquery.dataTables.js"></script>
 	<script src="<?php echo base_url();?>assets/plugins/datatables/media/js/dataTables.bootstrap.js"></script>
 	<script src="<?php echo base_url();?>assets/plugins/datatables/extensions/Responsive/js/dataTables.responsive.min.js"></script>
-
+	<!--Bootbox Modals [ OPTIONAL ]-->
+	<script src="<?php echo base_url();?>assets/plugins/bootbox/bootbox.min.js"></script>
+	<!--Modals [ SAMPLE ]-->
+	<script src="<?php echo base_url();?>assets/js/demo/ui-modals.js"></script>
 	<!--Custom JS for Ajax CRUD [ RECOMMENDED ]-->
 	<script type="text/javascript" src="<?php echo base_url();?>assets/js/custom_ajax_datatable.js"></script>
+	<script type="text/javascript" src="<?php echo base_url();?>assets/js/transaction.js"></script>
+	
 </body>
 </html>
